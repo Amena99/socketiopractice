bca5f891c12f9acdf482fc344a0e9cd6af3d4010
Amena Ahmed <am.ahmed09@gmail.com>
2019-05-23 15:22:29 -0500

    socket connection now works with client and server.

--- app.js
+++ app.js
@@ -2,46 +2,36 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-const cors = require("cors");
-
-// const allowedOrigins = "localhost:* 10.1.220.19:*";
-
-// const sio_server = io(server, {
-//     origins: allowedOrigins
-// })
-// io.origins(["http://10.1.220.19:4200"]);
 
 
-const corsOptions = {
-    origin: "http://10.1.220.19:4200",
-    credentials: true
-};
+//CORS middleware
 app.use((req, res, next) => {
     res.header({ "Access-Control-Allow-Origin": "*" })
     res.header({ "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE"});
     res.header({ "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"})
     next();
    });
-   
-app.use(cors());
 
-//setting static folder to '/node_modules' since we have npm to give us jQuery we want to be able to useit in our html. 
-
+//Routes
 app.use(express.static(__dirname + '/node_modules'));
 app.get('/', function(req, res, next){
     res.sendFile(__dirname + '/index.html');
 });
 
-
-
+//Socket.io connection
 io.on("connection", function(client){
+    //back end logging when client connects
     console.log("Client connected...");
 
     client.on("join", function(data){
-        console.log(data);
+        console.log("data", data);
+        client.emit("messages", "Hello from server.");
     });
 });
 
-server.listen(4200);
+//server listen with a callback
+server.listen(4200, () => {
+    console.log("I am running!");
+});
 
  
